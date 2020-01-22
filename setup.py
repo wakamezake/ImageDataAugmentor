cd3d9bc8a08077139e8f7da21bc896033d9f4ffd
kot.yuhara <kot.yuhara@trifort.jp>
2020-01-22 13:52:55 +0900

    init

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+LICENSE = 'MIT'
+with open('README.md') as f:
+    LONG_DESCRIPTION = f.read()
+setup(
+    name="ImageDataAugmentor",
+    version="0.0.0",
+    license=LICENSE,
+    long_description=LONG_DESCRIPTION,
+    install_requires=[
+        "tensorflow>=2.0.0",
+        "numpy>=1.18.1",
+        "opencv-python>=4.1.2.30",
+        "matplotlib>=3.1.2"],
+)
