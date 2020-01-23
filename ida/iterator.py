d368fab3e5fe815fd98f50420086dbf0119a2f6a
kot.yuhara <kot.yuhara@trifort.jp>
2020-01-23 16:57:06 +0900

    _get_batches_of_transformed_samples

--- ida/iterator.py
+++ ida/iterator.py
@@ -16,6 +16,7 @@ except ImportError:
     IteratorType = object
 
 from keras_preprocessing.image.utils import array_to_img, load_img
+from keras_preprocessing.image.iterator import BatchFromFilesMixin
 
 
 class Iterator(IteratorType):
@@ -126,89 +127,7 @@ class Iterator(IteratorType):
         raise NotImplementedError
 
 
-class BatchFromFilesMixin():
-    """Adds methods related to getting batches from filenames
-
-    It includes the logic to transform image files to batches.
-    """
-
-    def set_processing_attrs(self,
-                             image_data_generator,
-                             target_size,
-                             color_mode,
-                             data_format,
-                             save_to_dir,
-                             save_prefix,
-                             save_format,
-                             subset,
-                             interpolation):
-        """Sets attributes to use later for processing files into a batch.
-
-        # Arguments
-            image_data_generator: Instance of `ImageDataAugmentor`
-                to use for random transformations and normalization.
-            target_size: tuple of integers, dimensions to resize input images to.
-            color_mode: One of `"rgb"`, `"rgba"`, `"gray"`.
-                Color mode to read images.
-            data_format: String, one of `channels_first`, `channels_last`.
-            save_to_dir: Optional directory where to save the pictures
-                being yielded, in a viewable format. This is useful
-                for visualizing the random transformations being
-                applied, for debugging purposes.
-            save_prefix: String prefix to use for saving sample
-                images (if `save_to_dir` is set).
-            save_format: Format to use for saving sample images
-                (if `save_to_dir` is set).
-            subset: Subset of data (`"training"` or `"validation"`) if
-                validation_split is set in ImageDataAugmentor.
-            interpolation: Interpolation method used to
-                resample the image if the
-                target size is different from that of the loaded image.
-                Supported methods are `"cv2.INTER_NEAREST"`, `"cv2.INTER_LINEAR"`, `"cv2.INTER_AREA"`, `"cv2.INTER_CUBIC"`
-                and `"cv2.INTER_LANCZOS4"`
-                By default, `"cv2.INTER_NEAREST"` is used.
-        """
-        self.image_data_generator = image_data_generator
-        self.target_size = tuple(target_size)
-        if color_mode not in {'rgb', 'rgba', 'gray'}:
-            raise ValueError('Invalid color mode:', color_mode,
-                             '; expected "rgb", "rgba", or "gray".')
-        self.color_mode = color_mode
-        self.data_format = data_format
-        if self.color_mode == 'rgba':
-            if self.data_format == 'channels_last':
-                self.image_shape = self.target_size + (4,)
-            else:
-                self.image_shape = (4,) + self.target_size
-        elif self.color_mode == 'rgb':
-            if self.data_format == 'channels_last':
-                self.image_shape = self.target_size + (3,)
-            else:
-                self.image_shape = (3,) + self.target_size
-        else:
-            if self.data_format == 'channels_last':
-                self.image_shape = self.target_size + (1,)
-            else:
-                self.image_shape = (1,) + self.target_size
-        self.save_to_dir = save_to_dir
-        self.save_prefix = save_prefix
-        self.save_format = save_format
-        self.interpolation = interpolation
-        if subset is not None:
-            validation_split = self.image_data_generator._validation_split
-            if subset == 'validation':
-                split = (0, validation_split)
-            elif subset == 'training':
-                split = (validation_split, 1)
-            else:
-                raise ValueError(
-                    'Invalid subset name: %s;'
-                    'expected "training" or "validation"' % (subset,))
-        else:
-            split = None
-        self.split = split
-        self.subset = subset
-
+class CustomBatchFromFilesMixin(BatchFromFilesMixin):
     def _get_batches_of_transformed_samples(self, index_array):
         """Gets a batch of transformed samples.
 
@@ -265,26 +184,3 @@ class BatchFromFilesMixin():
             return batch_x, batch_y
         else:
             return batch_x, batch_y, self.sample_weight[index_array]
-
-    @property
-    def filepaths(self):
-        """List of absolute paths to image files"""
-        raise NotImplementedError(
-            '`filepaths` property method has not been implemented in {}.'
-                .format(type(self).__name__)
-        )
-
-    @property
-    def labels(self):
-        """Class labels of every observation"""
-        raise NotImplementedError(
-            '`labels` property method has not been implemented in {}.'
-                .format(type(self).__name__)
-        )
-
-    @property
-    def sample_weight(self):
-        raise NotImplementedError(
-            '`sample_weight` property method has not been implemented in {}.'
-                .format(type(self).__name__)
-        )
